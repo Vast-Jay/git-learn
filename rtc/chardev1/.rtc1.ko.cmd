46db76703abb23cdb3cd5c7f19cdbb39318fe3d0
Vast-Jay <76048821+Vast-Jay@users.noreply.github.com>
2023-09-04 14:23:10 +0800

    Delete rtc/chardev1 directory

--- rtc/chardev1/.rtc1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/zhaixue/git-linux/rtc/chardev1/rtc1.ko := arm-linux-gnueabi-ld -r  -EL  --build-id  -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds -o /home/zhaixue/git-linux/rtc/chardev1/rtc1.ko /home/zhaixue/git-linux/rtc/chardev1/rtc1.o /home/zhaixue/git-linux/rtc/chardev1/rtc1.mod.o;  true
--- rtc/chardev1/.rtc1.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/zhaixue/git-linux/rtc/chardev1/rtc1.mod := { echo  /home/zhaixue/git-linux/rtc/chardev1/rtc1.o; echo; } > /home/zhaixue/git-linux/rtc/chardev1/rtc1.mod
