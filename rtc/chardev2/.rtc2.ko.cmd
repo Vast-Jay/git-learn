05977cf961c87bea7cae67d825a62283b2cd4686
Vast-Jay <76048821+Vast-Jay@users.noreply.github.com>
2023-09-04 14:22:54 +0800

    Delete rtc/chardev2 directory

--- rtc/chardev2/.rtc2.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/zhaixue/git-linux/rtc/chardev2/rtc2.ko := arm-linux-gnueabi-ld -r  -EL  --build-id  -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds -o /home/zhaixue/git-linux/rtc/chardev2/rtc2.ko /home/zhaixue/git-linux/rtc/chardev2/rtc2.o /home/zhaixue/git-linux/rtc/chardev2/rtc2.mod.o;  true
--- rtc/chardev2/.rtc2.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/zhaixue/git-linux/rtc/chardev2/rtc2.mod := { echo  /home/zhaixue/git-linux/rtc/chardev2/rtc2.o; echo; } > /home/zhaixue/git-linux/rtc/chardev2/rtc2.mod
